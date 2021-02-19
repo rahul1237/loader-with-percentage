c6699b987240d3553b0f0a53b99c0b4c22d7d0d9
Rahul Mahajan <rahulmahajan98645@gmail.com>
2021-02-20 00:18:38 +0530

    loader effect with percentage added

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const op = setInterval(a,20)
+const duration = setInterval(main_fun,20)
 
-function a(){
+function main_fun(){
     const precentage = document.querySelector('.percentage')
     const line = document.querySelector('.line')
 
-    let b = window.getComputedStyle(line,':before').getPropertyValue('width')
-    b = Math.floor((parseInt(b)/9.5)*2)
-    precentage.innerHTML = b + '%'
-    console.log(b);
-    if(b==100){
-        clearInterval(op)
+    let percentage_val = window.getComputedStyle(line,':before').getPropertyValue('width')
+    percentage_val = Math.floor((parseInt(percentage_val)/9.5)*2)
+    precentage.innerHTML = percentage_val + '%'
+    if(percentage_val==100){
+        clearInterval(duration)
     }
 }
